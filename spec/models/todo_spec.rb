085a6a9261c88c3fefeb086067a1725b2f2e341b
sherlyc <sherlyc@gmail.com>
2018-06-02 17:23:33 +1200

    add todo spec

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # ensure Todo model has a 1:m relationship with the Item model
+  it { should have_many(:items).dependant(:destroy) }
+  # ensure columns title and created_by are present before saving
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:created_by) }
 end
