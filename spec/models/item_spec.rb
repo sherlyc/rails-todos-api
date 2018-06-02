a27d3816f604b1cf5db8f9fa5620797bb09ee07d
sherlyc <sherlyc@gmail.com>
2018-06-02 17:33:51 +1200

    add test for todo and item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # ensure an item record belongs to a single todo record
+  it { should belong_to(:todo) }
+  # ensure column name is present before saving
+  it { should validate_presence_of(:name) }
 end
